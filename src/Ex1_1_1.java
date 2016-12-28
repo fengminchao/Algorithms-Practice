021d6208669b078d40b300ad0683e99195685e28
ybao <ybaovv@gmail.com>
2016-12-28 15:46:37 +0800

    init

---
+++ src/Ex1_1_1.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * Created by ybao on 16/12/28.
+ */
+public class Ex1_1_1 {
+
+    public static void main(String[] args) {
+        System.out.println(2.0e-6 * 100000000.1 + "");
+        boolean b = true && false || true && true;
+        System.out.println(b + "");
+    }
+}

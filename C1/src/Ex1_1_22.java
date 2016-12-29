bdbb73f20917d1de20185b13cafffef57d5746fa
ybao <ybaovv@gmail.com>
2016-12-29 22:13:57 +0800

    update

--- C1/src/Ex1_1_22.java
+++ C1/src/Ex1_1_22.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 /**
  * Created by ybao on 16/12/29.
  */
 public class Ex1_1_22 {
+    public static void main(String[] args) {
+        int[] array = {1, 3, 5, 7, 9};
+        int location = rank(3, array);
+        if (location < 0)
+            StdOut.print("no found");
+        else
+            StdOut.print("location: " + location);
+
+    }
+
+    public static int rank(int key, int[] a) {
+        return rank(key, a, 0, a.length - 1, 0);
+    }
+
+    public static int rank(int key, int[] a, int lo, int hi, int dep) {
+        int mid = lo + (hi - lo) / 2;
+        for (int i = 0; i < dep; i++)
+            StdOut.print(" ");
+        StdOut.println("low:" + lo + " height:" + hi);
+        if (lo == hi){
+            return -1;
+        }
+        if (a[mid] == key) {
+            return mid;
+        } else if (a[mid] > key) {
+            return rank(key, a, lo, mid, ++dep);
+        } else {
+            return rank(key, a, mid, hi, ++dep);
+        }
+    }
 }
---
+++ C1/src/Ex1_2_6.java
@@ -0,0 +1,21 @@
+/**
+ * Created by ybao on 16/12/29.
+ */
+public class Ex1_2_6 {
+
+    public static void main(String[] args) {
+        String s1 = "actbyt";
+        String s2 = "ytactb";
+
+        if (s1.length() == s2.length()){
+            s1 = s1 + s1;
+            if (s1.indexOf(s2) < s2.length() && s1.indexOf(s2) > 0){
+                System.out.println("s1 is circular rotation to s2");
+            }else {
+                System.out.println("they are not circular rotation");
+            }
+        }else {
+            System.out.println("they are not circular rotation");
+        }
+    }
+}
